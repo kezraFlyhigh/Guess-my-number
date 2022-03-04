18c7aeb7c6dab869c068470b53b36f298b89ea32
kezraFlyhigh <97551724+kezraFlyhigh@users.noreply.github.com>
2022-03-04 22:57:35 +0100

    Just some playing around with js
    I'm just learning and uploading some files from the course;
    https://www.udemy.com/course/the-complete-javascript-course/

---
+++ script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/*
+console.log(document.querySelector(`.message`).textContent);
+
+document.querySelector(`.message`).textContent = `🎉Correct number!`;
+document.querySelector(`.number`).textContent = 13;
+document.querySelector(`.score`).textContent = 10;
+
+
+document.querySelector(`.guess`).value = 23;
+console.log(document.querySelector(`.guess`).value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.number`).style.width = `15rem`;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+});
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector(`.message`).textContent = `🛑 no number!`;
+    // When the answer is correct
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `🎉 Correct number!`;
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+    document.querySelector(`.number`).textContent = secretNumber;
+    document.querySelector(`.number`).style.width = `30rem`;
+    if (highscore < score) {
+      highscore = score;
+      document.querySelector(`.highscore`).textContent = highscore;
+    }
+    // When the answer is lower than number
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `📈 Too high`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `💥You lost the game!😭`;
+      document.querySelector(`.score`).textContent = 0;
+    }
+    //When the answer is higher than number
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `📉 Too low`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `💥You lost the game!😭`;
+      document.querySelector(`.score`).textContent = 0;
+    }
+  }
+});
